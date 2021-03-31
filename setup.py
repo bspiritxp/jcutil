04e82603a8789ba056813422dbe261dbc9621d20
Jochen.He <thjl@hotmail.com>
2021-03-31 18:55:08 +0800

    update version to 0.0.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf8') as fh:
 
 setuptools.setup(
     name='jcutil',
-    version='0.0.3',
+    version='0.0.4',
     author='Jochen.He',
     author_email='thjl@hotmail.com',
     description='some python util tools in one package',
